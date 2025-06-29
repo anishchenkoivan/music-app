ea96a76b6dc485f67630b85f68a5e907f2f020ad
Ivan Anishchenko <ivananisenko08@gmail.com>
2025-06-29 23:06:15 +0300

    Basic restructure

--- main-service/src/main/java/com/musicapp/mainservice/dto/AuthDto.java
+++
@@ -1,6 +0,0 @@
-package com.musicapp.mainservice.dto;
-
-import java.util.UUID;
-
-public record AuthDto(UUID id, String token) {
-}
--- main-service/src/main/java/com/musicapp/mainservice/dto/request/UserUpdateRequest.java
+++
@@ -1,6 +0,0 @@
-package com.musicapp.mainservice.dto.request;
-
-import com.musicapp.mainservice.dto.UserDetailsDto;
-
-public record UserUpdateRequest(UserDetailsDto userDetails, String password) {
-}
